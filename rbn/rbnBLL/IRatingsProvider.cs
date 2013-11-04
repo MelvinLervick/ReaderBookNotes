1b7449873dd6e251cdbda5b0546aaeef4f171c82
Melvin Lervick <melvin.lervick@infospace.com>
2013-11-03 19:39:46 -0800

    Add excluded files

---
+++ rbn/rbnBLL/IRatingsProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using rbnBLL.Models;
+
+namespace rbnBLL
+{
+  public interface IRatingsProvider
+  {
+    IEnumerable<Ratings> GetAuthorRatingsList( int authorId );
+    void CreateAuthorRating( Ratings newRating );
+  }
+}
