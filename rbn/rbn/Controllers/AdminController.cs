c9f3ea2bbe618c61e2119df5f6ab7aafe4bb25d7
Melvin Lervick <melvin.lervick@infospace.com>
2014-01-25 09:12:08 -0800

    Admin continued

--- rbn/rbn/Controllers/AdminController.cs
+++ rbn/rbn/Controllers/AdminController.cs
@@ -3,24 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using rbn.Models;
+using rbn.Providers;
+using rbn.Providers.RbnBLL;
 
 namespace rbn.Controllers
 {
     public class AdminController : Controller
     {
+        private IReaderAliasProvider readerAliasProvider;
+        internal IReaderAliasProvider ReaderAliasProvider
+        {
+            get
+            {
+                return readerAliasProvider ?? (readerAliasProvider = new ReaderAliasProvider());
+            }
+        }
+        private IUserAccountProvider accountProvider;
+        internal IUserAccountProvider AccountProvider
+        {
+            get
+            {
+                return accountProvider ?? (accountProvider = new UserAccountProvider());
+            }
+        }
         //
         // GET: /Admin/
 
         public ActionResult Index()
         {
+            if (Roles.GetRolesForUser(User.Identity.Name).Contains("Administrator"))
+            {
+                ViewBag.ReaderId = FillReaderAliasList();
+            }
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(AuthorModel model, string admin)
+        public ActionResult Index(string admin)
         {
             return View();
+        }
+
+        private IEnumerable<SelectListItem> FillReaderAliasList()
+        {
+            List<SelectListItem> items = (from value in ReaderAliasProvider.GetReaderAliases()
+                select new SelectListItem
+                {
+                    Text = value.Alias,
+                    Value = value.ReaderId.ToString()
+                }).ToList();
+
+            return items;
         }
     }
 }
