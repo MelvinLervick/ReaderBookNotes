bbd1b941f3196bcde6f726bb9d19787d454515f9
Melvin Lervick <melvin.lervick@infospace.com>
2014-01-25 14:43:12 -0800

    Add other files

---
+++ rbn/LibraryDAO/Models/Author.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace LibraryDAO.Models
+{
+    public class Author
+    {
+        public string Name
+        {
+            get { return string.Format( "{2}, {0} {1}", FirstName, MiddleName, LastName ); }
+
+            set
+            {
+                var name = value;
+                if ( name.Contains( "," ) )
+                {
+                    var nameList= name.Split( ',' );
+                    LastName = nameList[0];
+
+                    var firstAndMiddle = new ArrayList();
+                    firstAndMiddle.AddRange( nameList[1].Split( ' ' ) );
+                    FirstName = firstAndMiddle[0].ToString();
+                    firstAndMiddle.RemoveAt( 0 );
+                    if ( firstAndMiddle.Count > 0 )
+                    {
+                        MiddleName = firstAndMiddle.ToString();
+                    }
+                }
+                else
+                {
+                    var nameList = new ArrayList();
+                    nameList.AddRange(name.Split( ' ' ));
+                    FirstName = nameList[0].ToString();
+                    nameList.RemoveAt( 0 );
+                    if ( nameList.Count > 1 )
+                    {
+                        MiddleName = nameList[0].ToString();
+                        nameList.RemoveAt( 0 );
+                    }
+                    LastName = nameList.ToString();
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string MiddleName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ rbn/LibraryDAO/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿namespace LibraryDAO.Models
+{
+    public class Book
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Isbn10
+        {
+            get;
+            set;
+        }
+
+        public string Isbn13
+        {
+            get;
+            set;
+        }
+
+        public Author Author
+        {
+            get;
+            set;
+        }
+    }
+}
